20a68b1c45eb4e3483e49b1b38f86301a8f90210
huynd <huynd@unit.com.vn>
2025-10-05 15:18:12 +0700

    cập nhật thông số và giao diện

--- app/src/main/java/com/seo/visualexhibition/ui/transform/TransformFragment.kt
+++ app/src/main/java/com/seo/visualexhibition/ui/transform/TransformFragment.kt
@@ -65,21 +65,7 @@ class TransformFragment : Fragment() {
 
         private val drawables = listOf(
             R.drawable.avatar_1,
-            R.drawable.avatar_2,
-            R.drawable.avatar_3,
-            R.drawable.avatar_4,
-            R.drawable.avatar_5,
-            R.drawable.avatar_6,
-            R.drawable.avatar_7,
-            R.drawable.avatar_8,
-            R.drawable.avatar_9,
-            R.drawable.avatar_10,
-            R.drawable.avatar_11,
-            R.drawable.avatar_12,
-            R.drawable.avatar_13,
-            R.drawable.avatar_14,
-            R.drawable.avatar_15,
-            R.drawable.avatar_16,
+            R.drawable.avatar_2
         )
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransformViewHolder {
